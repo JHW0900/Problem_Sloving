1753190e3bb271e79f84f6bd0a03aa5d5a4d5cbd
jhw0900 <jhw0900@gmail.com>
2021-01-25 20:32:36 +0900

    1이 될 때까지 (99) add

---
+++ 이.코.테/그리디/4.py
@@ -0,0 +1,31 @@
+# 1이 될 때까지
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+
+# -1
+# N / K
+
+count = 0
+while n != 1:
+  if n % k == 0:
+    count += 1
+    n //= k
+  else:
+    count += 1
+    n -= 1
+
+# while True:
+#   target = n // k * k
+#   count += n - target
+#   n = target
+
+#   if n < k:
+#     break
+
+#   count += 1
+#   n //= k
+
+# count -= 1
+print(count) 
