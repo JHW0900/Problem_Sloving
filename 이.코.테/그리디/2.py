cfaa607257162ee365cba8c2395230665e026df9
jhw0900 <jhw0900@gmail.com>
2021-01-25 20:07:00 +0900

    update 큰 수의 법칙 (답안 예시)

--- 이.코.테/그리디/2.py
+++ 이.코.테/그리디/2.py
@@ -6,13 +6,18 @@ N, M, K = map(int, input().split())
 data = list(map(int, input().split()))
 data = sorted(data, reverse=True)
 
-i = 0
-summary = 0
-while i < M:
-  if K+1 > M-i:
-    summary += data[0]*(M-i)
-    break
-  summary += (data[0]*K + data[1])
-  i += 4
+# i = 0
+# summary = 0
+# while i < M:
+#   if K+1 > M-i:
+#     summary += data[0]*(M-i)
+#     break
+#   summary += (data[0]*K + data[1])
+#   i += 4
+
+# 답안 예시
+count = M//(K+1)*K + M%(K+1)    # 가장 큰 수의 등장 횟수를 먼저 구함
+summary = count * data[0]
+summary += (M - count) * data[1]
 
 print(summary)
