c561f998532c69f0df55b351aad5649a5beb93fd
jhw0900 <jhw0900@gmail.com>
2021-02-07 19:25:58 +0900

    홀수 (2576) add

---
+++ BOJ/2576.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    int sum = 0, min = 100;
+
+    for(int i = 0; i < 7; i++){
+        cin >> n;
+        if (n % 2 != 0){
+            sum += n;
+            if (n < min) min = n;
+        }
+    }
+
+    if (sum == 0) cout << -1;
+    else cout << sum << '\n' << min;
+
+    return 0;
+}
