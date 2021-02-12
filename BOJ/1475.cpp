b85b43f488f3d1cf549cd3a1c9ed6c980e516ec5
jhw0900 <jhw0900@gmail.com>
2021-02-13 05:38:41 +0900

    add 방 번호(1475)

---
+++ BOJ/1475.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    int amt = 0;
+    int num[10] = { 0 };
+    cin >> n;
+
+    do {
+        num[n % 10]++;
+        n /= 10;
+    } while(n);
+
+    for (int i = 0; i < 10; i++) {
+        if (i == 6 || i == 9) continue;
+        else if (num[i] > amt) amt = num[i];
+    }
+
+    int tmp = (num[6] + num[9]) / 2;
+    if ((num[6] + num[9]) % 2 == 0) {
+        if (tmp > amt)
+            amt = tmp;
+    }
+    else {
+        tmp += 1;
+        if (tmp > amt)
+            amt = tmp;
+    }
+
+    cout << amt;
+
+    return 0;
+}
