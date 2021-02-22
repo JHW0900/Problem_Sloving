6fad7ef1b7a5036ab159783a1bb16ee2863c996c
jhw0900 <jhw0900@gmail.com>
2021-02-22 16:06:41 +0900

    add 키로거(5397)

---
+++ BOJ/5397.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int tc;
+    cin >> tc;
+
+    while(tc--){
+        list<char> L;
+        list<char>::iterator t = L.begin();
+        string l;
+        cin >> l;
+        for(char e : l){
+            if(e == '<'){
+                if(t != L.begin()) t--;
+                else continue;
+            } 
+            else if(e == '>'){
+                if(t != L.end()) t++;
+                else continue;
+            }
+            else if(e == '-'){
+                if(t != L.begin()){
+                    t--;
+                    t = L.erase(t);
+                }
+                else continue; 
+            }
+            else if(L.size() == 0){
+                L.push_back(e);
+                t = L.begin();
+                t++;
+            }
+            else {
+                L.insert(t, e);
+            }
+        }
+        for(auto it : L) cout << it;
+        cout << '\n';
+    }
+
+    return 0;
+}
