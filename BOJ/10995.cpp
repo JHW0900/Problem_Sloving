0d568c64c4076d802472549f2fa2d217c4dcd966
jhw0900 <jhw0900@gmail.com>
2021-03-07 19:59:20 +0900

    add 별 찍기 - 20(10995)

---
+++ BOJ/10995.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 0; j < n; j++){
+            if (i % 2 == 1)
+                cout << "* ";
+            else
+                cout << " *";
+        }
+        cout << '\n';
+    }
+    return 0;
+}
