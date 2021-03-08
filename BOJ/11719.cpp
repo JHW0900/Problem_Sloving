d0b3408e40e3e3556b3db21b7b4fb9964ad4e6ab
jhw0900 <jhw0900@gmail.com>
2021-03-08 16:03:32 +0900

    add 그대로 출력하기 2(11719)

---
+++ BOJ/11719.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    string str;
+    for(int i = 0; i < 100; i++){
+        getline(cin, str);
+        cout << str << '\n';
+    }
+    return 0;
+}
