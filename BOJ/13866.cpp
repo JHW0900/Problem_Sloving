4ce656380a092cba71ba1d30a4a625f46c06a11b
jhw0900 <jhw0900@gmail.com>
2021-04-01 18:56:37 +0900

    add 팀 나누기(13866)

---
+++ BOJ/13866.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int skill[4];
+    for(int i = 0; i < 4; i++) cin >> skill[i];
+
+    cout << (skill[3] + skill[0]) - (skill[1] + skill[2]);
+    return 0;
+}
